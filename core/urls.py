8691bb3bd3a6d2e688ffde6614841f92bf0e38fd
mahmoud_salama <salamozm346@gmail.com>
2022-04-25 01:21:59 +0200

    add post creation from form

--- core/urls.py
+++ core/urls.py
@@ -5,8 +5,9 @@ from .views import *
 
 urlpatterns = [
     path('', profile, name='profile'),
-    path('account_settings/', ProfileSettingUpdateView.as_view(), name='account_settings'),
     path('signup/', SignUpView.as_view(), name='signup'),
     path('login/', login_page, name='login'),
     path('logout/', logout_page, name='logout'),
+    path('account_settings/', ProfileSettingUpdateView.as_view(), name='account_settings'),
+    path('posts/new/', PostCreationView.as_view(), name='new_post'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- core/views.py
+++ core/views.py
@@ -4,13 +4,13 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.utils.decorators import method_decorator
 from .forms import *
-from .models import User
+from .models import *
 
 
 class SignUpView(CreateView):
     model = User
     form_class = SignUpForm
-    template_name = 'signup.html'
+    template_name = 'auth/signup.html'
 
     # method 1
     def get_success_url(self):
@@ -33,7 +33,7 @@ def login_page(request):
     if request.user.is_authenticated:
         return redirect('/')
     if request.method == 'GET':
-        return render(request, 'login.html')
+        return render(request, 'auth/login.html')
     elif request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
@@ -54,15 +54,26 @@ def logout_page(request):
 
 @login_required(login_url='/login')
 def profile(request):
-    return render(request, 'profile.html')
+    return render(request, 'home/profile.html')
 
 
 @method_decorator(login_required(login_url='/login'), name='dispatch')
 class ProfileSettingUpdateView(UpdateView):
     model = User
     form_class = ProfileSettingForm
-    template_name = 'account_settings.html'
+    template_name = 'home/account_settings.html'
     success_url = '/'
 
     def get_object(self, queryset=None):
         return User.objects.get(pk=self.request.user.pk)
+
+
+class PostCreationView(CreateView):
+    model = Post
+    fields = ['caption']
+    template_name = 'posts/new_post.html'
+    success_url = '/'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(PostCreationView, self).form_valid(form)
